4705babc552215815a8c86470532fce84c18f670
Daniil <dan98l@mail.ru>
2020-07-18 14:05:55 +0700

    vk

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Menu from './Menu/Menu';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Maths from './Menu/Subjects/Maths/Maths';
+import Physics from './Menu/Subjects/Physics/Physics';
+import History from './Menu/Subjects/History/History';
+import Chemistry from './Menu/Subjects/Chemistry/Chemistry';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Menu />
+        <Route exact path='/maths' component={Maths} />
+        <Route exact path='/physics' component={Physics} />
+        <Route exact path='/history' component={History} />
+        <Route exact path='/chemistry' component={Chemistry} />
+        <div className='text'>Formulas</div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
